35576fbb985aad64e3f289702c995f4fe423eb12
Ray Krueger <raykrueger@gmail.com>
2025-08-14 18:47:08 +0000

    docs: update copyright to use full name Ray Krueger
    Updated copyright notices across all source files and LICENSE:
    - LICENSE: Updated Apache License copyright notice
    - Source files: Updated copyright headers in lib.rs, main.rs
    - Examples: Updated copyright header in basic_usage.rs
    - Tests: Updated copyright header in integration_test.rs
    
    All copyright references now consistently use 'Ray Krueger' instead of 'rkrueger'

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 rkrueger
+// Copyright 2025 Ray Krueger
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
